c58767c4c50e205609f57f917a8727d5ede1c297
deemarc <deemarc@thaigertec.com>
2017-04-30 23:55:33 +0700

    restructure wtb and master class

--- masterClass.m
+++ masterClass.m
@@ -4,8 +4,8 @@ classdef masterClass
     
     properties
         masterState = wtbEnum.init
-        timeOutCounter = 0;
-        timeOut = 3,   %time out after 5 second
+        timeOutTimer = 0;
+        timeOutVal = 3,   %time out after 3 second
         
         wtbHandle = []
         cmdToSend = []
@@ -17,35 +17,57 @@ classdef masterClass
             obj.wtbHandle = wtbCmd;
             obj.wtbHandle.comName = comName;
             obj.wtbHandle = obj.wtbHandle.connect();
-            obj.wtbHandle = obj.wtbHandle.scanNetwork();
-            curMsg = wtbMsg.emptyWtbFrame;
-            curMsg.data(1) = 2;
-            for i = 2:length(obj.wtbHandle.compoTable)
-                curMsg.dest = obj.wtbHandle.compoTable(i).id;
-                obj.wtbHandle.txBuff{end+1} = wtbMsg.wtb2raw(curMsg);
-            end
-            obj.wtbHandle = obj.wtbHandle.wtbWrite();
-            obj.wtbHandle = obj.wtbHandle.wtbRead();
-            obj.wtbHandle = obj.wtbHandle.wtbUpdate(wtbEnum.read);
-            obj.wtbHandle = obj.wtbHandle.scanNetwork();
+            obj = obj.sendScanNetworkMsg();
+            %change state to scanNetwork and reset timer
+            obj.masterState = wtbEnum.scan;
+            obj.timeOutTimer = 0;
+            
+%             obj.wtbHandle = obj.wtbHandle.scanNetwork();
+%             curMsg = wtbMsg.emptyWtbFrame;
+%             curMsg.data(1) = 2;
+
+%             obj.wtbHandle = obj.wtbHandle.wtbWrite();
+%             obj.wtbHandle = obj.wtbHandle.wtbRead();
+%             obj.wtbHandle = obj.wtbHandle.wtbUpdate(wtbEnum.read);
+%             obj.wtbHandle = obj.wtbHandle.scanNetwork();
         end
         function obj = cycleTask(obj)
-            curMsg = wtbMsg.emptyWtbFrame;
-            curMsg.data(1) = 3;
-            obj = obj.wtbHandle.read();
-            obj.wtbHandle = obj.wtbHandle.wtbUpdate(wtbEnum.driving);
-            curMsg.dest = obj.wtbHandle.compoTable(3).id;
-            obj.wtbHandle.txBuff{end+1} = wtbMsg.wtb2raw(curMsg);
-            curMsg.dest = obj.wtbHandle.compoTable(4).id;
-            obj.wtbHandle.txBuff{end+1} = wtbMsg.wtb2raw(curMsg);
-            obj.wtbHandle = obj.wtbHandle.wtbWrite();
-            breVal = obj.wtbHandle.compoTable(3).value;
-            acceVal = obj.wtbHandle.compoTable(4).value;
+             
+%             obj = obj.wtbHandle.read();
+%             obj.wtbHandle = obj.wtbHandle.wtbUpdate(wtbEnum.driving);
+%             curMsg.dest = obj.wtbHandle.compoTable(3).id;
+%             obj.wtbHandle.txBuff{end+1} = wtbMsg.wtb2raw(curMsg);
+%             curMsg.dest = obj.wtbHandle.compoTable(4).id;
+%             obj.wtbHandle.txBuff{end+1} = wtbMsg.wtb2raw(curMsg);
+%             obj.wtbHandle = obj.wtbHandle.wtbWrite();
+%             breVal = obj.wtbHandle.compoTable(3).value;
+%             acceVal = obj.wtbHandle.compoTable(4).value;
+            obj.wtbHandle = obj.wtbHandle.wtbRead(obj.masterState);
             switch(obj.masterState)
                 case wtbEnum.scan
-                    obj = obj.wtbHandle.read();
-                    obj.wtbHandle = obj.wtbHandle.wtbUpdate(wtbEnum.driving);
-                    obj.wtbHandle = obj.wtbHandle.wtbWrite();
+                    if isempty(obj.wtbHandle.rxBuff)
+                       obj.timeOutTimer =  obj.timeOutTimer + 1;
+                       display('time out count up');
+                    else
+                       obj.timeOutTimer = 0;
+                       obj.wtbHandle = obj.wtbHandle.wtbUpdate(obj.masterState);
+                    end
+                    
+                    if obj.timeOutTimer >= obj.timeOutVal
+                        if isempty(obj.wtbHandle.chkBuff)
+                            obj.masterState = wtbEnum.read;
+                            obj.timeOutTimer = 0;
+                            %change to read functin state and read function
+                            %of all messages ID
+                            obj = obj.askAllFunc();
+                        else
+                            obj = obj.resendNonreplyMsg();
+                            obj.timeOutTimer = 0;
+                        end
+                    end
+%                     obj = obj.wtbHandle.read();
+%                     obj.wtbHandle = obj.wtbHandle.wtbUpdate(wtbEnum.driving);
+%                     obj.wtbHandle = obj.wtbHandle.wtbWrite();
 %                     if isempty(obj.wtbHandle.rxBuf)
 %                         obj.timeOutCounter = obj.timeOutCounter +1;
 %                     else
@@ -64,6 +86,23 @@ classdef masterClass
 %                         end
 %                     end
                 case wtbEnum.read
+                    if isempty(obj.wtbHandle.rxBuff)
+                       obj.timeOutTimer =  obj.timeOutTimer + 1;
+                       display('time out count up');
+                    else
+                       obj.timeOutTimer = 0;
+                       obj.wtbHandle = obj.wtbHandle.wtbUpdate(obj.masterState);
+                    end
+                    
+                    %if all message has function the we move to next state
+                    %else resend after timeout is reach
+                    if isempty(obj.wtbHandle.chkBuff)
+                        obj.masterState = wtbEnum.driving;
+                    elseif  obj.timeOutTimer >= obj.timeOutVal
+                        obj = obj.resendNonreplyMsg();
+                        obj.timeOutTimer;
+                    end
+
                 case wtbEnum.driving
                 otherwise
                         error('masterState has enter unknown state');
@@ -71,7 +110,26 @@ classdef masterClass
             
             
         end
-    
+        function obj = askAllFunc(obj)
+            curMsg = wtbMsg.emptyWtbFrame;
+            curMsg.data(1) = 2;
+            for i = 2:length(obj.wtbHandle.compoTable)
+                curMsg.dest = obj.wtbHandle.compoTable(i).id;
+                obj.wtbHandle.txBuff{end+1} = wtbMsg.wtb2raw(curMsg);
+            end
+            obj.wtbHandle = obj.wtbHandle.wtbWrite();
+        end
+        function obj = sendScanNetworkMsg(obj)
+            scanMsg = wtbMsg.scanNetworkMessage;
+            obj.wtbHandle.txBuff{1} = wtbMsg.wtb2raw(scanMsg);
+            obj.wtbHandle = obj.wtbHandle.wtbWrite();
+        end
+        function obj = resendNonreplyMsg(obj)
+            obj.wtbHandle.txBuff = obj.wtbHandle.chkBuff;
+            obj.wtbHandle.chkBuff = [];
+            obj.wtbHandle = obj.wtbHandle.wtbWrite();
+        end
+        
     
     %%%%%%%%%%%%%%% internal fucntion %%%%%%%%%%%%%%%%%%%%%%%%%%%
         
--- wtbCmd.m
+++ wtbCmd.m
@@ -49,7 +49,7 @@ classdef wtbCmd
             obj.txBuff = [];
         end
         
-        function obj = wtbRead(obj)
+        function obj = wtbRead(obj,masterState)
         %return rigid wtb message(only for master) to the caller
             if isempty(obj.comPort)
                 error('Com port is not init properly');
@@ -64,32 +64,32 @@ classdef wtbCmd
             obj.recvBuf(end+1:end+byteSize{1}) = fread(obj.comPort,byteSize{1});
             obj = byte2QWord(obj);
             obj = filterWtb(obj);
-            obj = txCrossCheck(obj);
+            obj = txCrossCheck(obj,masterState);
         end
-        function obj = scanNetwork(obj)
-            scanMsg = wtbMsg.scanNetworkMessage;
-            obj.txBuff{1} = wtbMsg.wtb2raw(scanMsg);
-            obj = obj.wtbWrite();
-            counter = 0;
-            Condition = true;
-            while Condition
-               % do stuff
-               obj = obj.wtbRead();
-               if isempty(obj.rxBuff)
-                   counter =  counter + 1;
-                   pause(obj.delay); %pause for 10 ms
-                   display('time out count up');
-               else
-                   counter = 0;
-                   obj = obj.assignId();
-               end
-               
-               if ( counter == obj.delayCount) % no msg for 50 ms, t
-                 Condition = false; 
-               end
-  
-            end
-        end
+%         function obj = scanNetwork(obj)
+%             scanMsg = wtbMsg.scanNetworkMessage;
+%             obj.txBuff{1} = wtbMsg.wtb2raw(scanMsg);
+%             obj = obj.wtbWrite();
+%             counter = 0;
+%             Condition = true;
+%             while Condition
+%                % do stuff
+%                obj = obj.wtbRead();
+%                if isempty(obj.rxBuff)
+%                    counter =  counter + 1;
+%                    pause(obj.delay); %pause for 10 ms
+%                    display('time out count up');
+%                else
+%                    counter = 0;
+%                    obj = obj.assignId();
+%                end
+%                
+%                if ( counter == obj.delayCount) % no msg for 50 ms, t
+%                  Condition = false; 
+%                end
+%   
+%             end
+%         end
         function obj = assignId(obj)
         % take new message in check wether it is message reply from network scan or set id
         % - reply from scan: check if it is new id or not set Id in compoTables
@@ -99,29 +99,7 @@ classdef wtbCmd
 
                 curWtbMsg = wtbMsg.raw2wtb(newMsgArr{i});
                 dec2hex(curWtbMsg.src)
-                if curWtbMsg.data(1) == 1
-                    disp('reply set');
-                % - reply from set: 
-                    obj.compoTable(curWtbMsg.data(2)+1).idOk = 1;
-%                     obj.idOK = obj.idOK +1;
-                elseif curWtbMsg.data(1) == 0
-                    disp('reply scan')
-                % - reply from set ID: update it in comPotable.idOK and count up idOk
-                        obj.compoTable(end+1).id = obj.compoTable(end).id +1;
-                        obj.compoTable(end).tmpId = curWtbMsg.src;
-                        disp(obj.compoTable(end).id)
-                        disp(obj.compoTable(end).tmpId)
-                        curWtbMsg.dest = curWtbMsg.src;
-                        curWtbMsg.src  = 0;
-                        curWtbMsg.ctrl = 1;
-                        curWtbMsg.data(1) = 1;
-                        curWtbMsg.data(2) = obj.compoTable(end).id;
-                        obj.txBuff{1} = wtbMsg.wtb2raw(curWtbMsg);
-                        obj = obj.wtbWrite();
-                else
-                    warning('recving incorrect response when scanning and assign ID');
-                end
-
+               
             end
         end
 
@@ -163,16 +141,40 @@ classdef wtbCmd
 %         end
 %         
         function obj = wtbUpdate(obj, masterState)
-            for i=2:length(obj.rxBuff)
-                curMsg = wtbMsg.raw2wtb(obj.rxBuff{i});
+            for i=1:length(obj.rxBuff)
+                curWtbMsg = wtbMsg.raw2wtb(obj.rxBuff{i});
                 
                 switch(masterState)
+                    case wtbEnum.scan
+                         if curWtbMsg.data(1) == 1
+%                           disp('reply set');
+                            % - reply from set: 
+                            obj.compoTable(curWtbMsg.data(2)+1).idOk = 1;
+                            %   obj.idOK = obj.idOK +1;
+                        elseif curWtbMsg.data(1) == 0
+                            disp('reply scan')
+                            % - reply from set ID: update it in comPotable.idOK and count up idOk
+                            obj.compoTable(end+1).id = obj.compoTable(end).id +1;
+                            obj.compoTable(end).tmpId = curWtbMsg.src;
+                            disp(obj.compoTable(end).id)
+                            disp(obj.compoTable(end).tmpId)
+                            curWtbMsg.dest = curWtbMsg.src;
+                            curWtbMsg.src  = 0;
+                            curWtbMsg.ctrl = 1;
+                            curWtbMsg.data(1) = 1;
+                            curWtbMsg.data(2) = obj.compoTable(end).id;
+                            obj.txBuff{1} = wtbMsg.wtb2raw(curWtbMsg);
+                            obj = obj.wtbWrite();
+                        else
+                            warning('recving incorrect response when scanning and assign ID');
+                        end
+
                     case wtbEnum.read
-                        obj.compoTable(curMsg.src + 1).function = ...
-                        curMsg.data(1) + 512*curMsg.data(2);
+                        obj.compoTable(curWtbMsg.src + 1).function = ...
+                        512*curWtbMsg.data(1) + curWtbMsg.data(2);
                     case wtbEnum.driving
-                        obj.compoTable(curMsg.src + 1).value = ...
-                            curMsg.data(1) + 32*curMsg.data(2);
+                        obj.compoTable(curWtbMsg.src + 1).value = ...
+                            512*curWtbMsg.data(1) + curWtbMsg.data(2);
                 end
                 
             end
@@ -212,17 +214,18 @@ classdef wtbCmd
         
         end
         
-        function obj = txCrossCheck(obj)
+        function obj = txCrossCheck(obj,masterState)
         %check if the transmitted message got reply or not
         %normal case: check Dest Id of sent message with src ID of received
         %             message
         %setId case : need to ckeck with setted Id instead(id will be changed)
         
             for i=1:length(obj.chkBuff)
-                if obj.chkBuff{i}(6) == 1
+                
+                if masterState == wtbEnum.init || masterState == wtbEnum.scan
                     chkIdx = 7; % setted ID
                 else
-                    chkIdx = 4; % src ID
+                    chkIdx = 2; % src ID
                 end
                 for j=1:length(obj.rxBuff)
                     disp('rxBufId');
